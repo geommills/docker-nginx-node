5bbbe1fb1cd18d26fffc9cceb264e2d41d7f2aef
Mike Mills <mmills@geoengineers.com>
2017-09-13 13:53:14 -0700

    console log actual endpoint

--- nodejs/index.js
+++ nodejs/index.js
@@ -7,4 +7,4 @@ var server = http.createServer(function (request, response){
 
 server.listen(3000);
 
-console.log("Server running at http://127.0.0.1:3000/");
+console.log("Server running at http://127.0.0.1/");
